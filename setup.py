bbdde6a7c78e66e02f0fe9623c5c931dae534220
abhijitpal1247 <abhijit@samasra.net>
2023-05-14 01:51:37 +0530

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='interior_design_controlnet',
       version='0.1',
       description='Testing installation of Package',
       url='#',
       author='auth',
       author_email='author@email.com',
+      install_requires=required,
       license='MIT',
       packages=['src'],
       zip_safe=False)
