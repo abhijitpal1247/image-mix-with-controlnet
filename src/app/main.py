3d48e228f16730f67ec3916e27ecea45b2b70c87
abhijitpal1247 <abhijit@samasra.net>
2023-05-14 01:37:57 +0530

    made a lot of changes for captions and image generation logic

--- src/app/main.py
+++ src/app/main.py
@@ -4,9 +4,8 @@ from src.models.controlnet_generator import generate_stylized_image
 from PIL import Image
 
 
-def upload_file():
-
-    image_columns = st.columns((2, 1, 2), gap="medium")
+def run():
+    image_columns = st.columns((1, 1), gap="medium")
     with image_columns[0]:
         content_file = st.file_uploader("Upload content file", type=['jpg', 'png', 'jpeg'])
         show_file_content = st.empty()
@@ -17,7 +16,7 @@ def upload_file():
             if isinstance(content_image, bytes):
                 show_file_content.image(content_file)
 
-    with image_columns[2]:
+    with image_columns[1]:
         style_file = st.file_uploader("Upload style file", type=['jpg', 'png', 'jpeg'])
         show_file_style = st.empty()
         if not style_file:
@@ -26,27 +25,23 @@ def upload_file():
             style_image = style_file.getvalue()
             if isinstance(style_image, bytes):
                 show_file_style.image(style_image)
-    button_columns = st.columns((1, 1, 1), gap="medium")
-    with button_columns[1]:
-        if style_file:
-            generate_captions_button = st.button("Compute styles/captions from the style image")
-            if generate_captions_button:
-                with st.spinner('Wait for it...'):
-                    if style_file:
-                        captions = generate_captions(style_file)
-                st.success('Done')
-                options = st.multiselect(
-                    'Select relevant captions/styles',
-                    captions.split(','), captions.split(','))
-                if options:
-                    generate_images_button = st.button("Generate image for the given style and content")
-                    if generate_images_button:
-                        if content_file:
-                            with st.spinner('Wait for it...'):
-                                generated_image = generate_stylized_image(', '.join(options), content_file)
-                            st.success('Done')
-                            st.image(generated_image)
+    show_gen_image = st.empty()
+    with st.sidebar:
+        caption_generate_button = st.button('Generate Captions/Styles')
+        if "options" not in st.session_state:
+            st.session_state.options = []
+        if caption_generate_button:
+            with st.spinner('Generating Captions'):
+                caption_generate_button.disabled = True
+                st.session_state.options = generate_captions(style_file)
+                caption_generate_button.disabled = False
+        options_multiselect = st.multiselect('Please select the captions/styles', st.session_state.options,
+                                             keys='selected')
+        generate_image_button = st.button('Generate stylized image')
+        if generate_image_button and options_multiselect:
+            generated_image = generate_stylized_image(', '.join(options_multiselect), content_file)
+            show_gen_image.image(generated_image, caption='generated image')
 
 
 if __name__ == '__main__':
-    upload_file()
+    run()
